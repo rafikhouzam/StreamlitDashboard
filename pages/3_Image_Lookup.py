0322973c93dcaace22390790b4e0c65a22d8adb3
Aneri Jewels <rafi@Aneris-iMac.local>
2025-05-07 16:38:16 -0400

    added 3rd page for Image Lookup (in development)

---
+++ pages/3_Image_Lookup.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Image Lookup",
+    page_icon="",
+    layout="wide"
+    )
+
+st.title("Image Lookup")
+
+st.markdown("""
+👋 This tool is under construction.
+
+Soon, you'll be able to:
+- Search for product images by style number
+- Search for products just by description
+
+Thanks for your patience!
+""")
+
+# Disabled input fields and button
+st.text_input(
+    "Search by Style Number or Description", 
+    placeholder="'e.g. R123456', '14K Engagement Ring Halo Oval'", 
+    disabled=True)
+st.button("Search", disabled=True)
