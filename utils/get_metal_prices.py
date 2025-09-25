ab811bd9baa0cf1811253a6f5121f0ed5be00b3b
Rafi Khouzam <khouzamrafi@gmail.com>
2025-09-25 10:09:58 -0400

    chore: update ttl cache to 24hrs for metalpriceAPI

--- utils/get_metal_prices.py
+++ utils/get_metal_prices.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 METALPRICE_API_KEY = st.secrets.get("METALPRICE_API_KEY")
 
-@st.cache_data(ttl=43200)  # cache for 12 hours
+@st.cache_data(ttl=86400)  # cache for 12 hours
 def get_metal_prices():
     url = f"https://api.metalpriceapi.com/v1/latest?api_key={METALPRICE_API_KEY}&base=USD&currencies=XAG,XAU,XPT,XPD"
     response = requests.get(url)
--- utils/navbar.py
+++ utils/navbar.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from utils.get_metal_prices import get_metal_prices
+from datetime import datetime
 
 def navbar():
     rates = get_metal_prices()
@@ -14,3 +15,7 @@ def navbar():
     cols[1].metric("Silver (USD)", f"${silver:,.2f}")
     cols[2].metric("Platinum (USD)", f"${platinum:,.2f}")
     cols[3].metric("Palladium (USD)", f"${palladium:,.2f}")
+
+    # optional: show when these were last pulled from API
+    last_updated = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    st.caption(f"⏱ Last updated: {last_updated} (API refreshes once daily)")
